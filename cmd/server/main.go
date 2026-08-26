098f9bd0e48f6b3c547267b1963ca294b9e77a7b
Ignacio Senra <isenras@hotmail.com>
2026-08-25 22:22:40 -0300

    Initial setup RabbitMQ container + message server generator

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connection_str := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connection_str)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ successfully.")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Failed to open a channel:", err)
+		return
+	}
+	defer channel.Close()
+	fmt.Println("Channel opened successfully.")
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		fmt.Println("Failed to publish JSON:", err)
+		return
+	}
+
+	// Create a channel to receive OS signals
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
+
+	// Wait for an interrupt signal
+	<-sigchan
+	fmt.Println("Shutting down Peril server...")
+
 }
